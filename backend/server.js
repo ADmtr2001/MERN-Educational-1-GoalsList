8c610cc475c860584ae220aab753bce54523b742
Arsen Dmitrenko <arsen.dmitrenko1994@gmail.com>
2022-04-08 01:24:50 +0300

    add basic routing

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,17 @@
-console.log('Hello World');
+const express = require('express');
+const dotenv = require('dotenv').config();
+const port = process.env.PORT ||  5000;
+
+// express
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+// routers
+const goalRouter = require('./routes/goalRoutes');
+
+app.use('/api/goals', goalRouter);
+
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
