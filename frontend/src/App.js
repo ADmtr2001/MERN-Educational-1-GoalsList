86841bf880bf6e101a0cd99a5556f1949463e006
Arsen Dmitrenko <arsen.dmitrenko1994@gmail.com>
2022-04-10 01:37:18 +0300

    finish project

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,27 @@
-import React from 'react';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import {ToastContainer} from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
-import Dashboard from "./pages/Dashboard";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import Header from "./components/Header";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import Header from './components/Header'
+import Dashboard from './pages/Dashboard'
+import Login from './pages/Login'
+import Register from './pages/Register'
 
-const App = () => {
-	return (
-		<>
-			<Router>
-				<div className='container'>
-					<Header/>
-					<Routes>
-						<Route path='/' element={<Dashboard/>}/>
-						<Route path='/login' element={<Login/>}/>
-						<Route path='/register' element={<Register/>}/>
-					</Routes>
-				</div>
-			</Router>
-			<ToastContainer/>
-		</>
-	);
-};
+function App() {
+  return (
+    <>
+      <Router>
+        <div className='container'>
+          <Header />
+          <Routes>
+            <Route path='/' element={<Dashboard />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+          </Routes>
+        </div>
+      </Router>
+      <ToastContainer />
+    </>
+  )
+}
 
-export default App;
+export default App
